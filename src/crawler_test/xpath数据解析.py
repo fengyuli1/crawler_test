689c35bc8d2adf730567a15538f6bf948808fdb7
fengyuli <270676153@qq.com>
2023-06-15 21:36:24 +0800

    修改pdm版本

--- src/crawler_test/xpath数据解析.py
+++ src/crawler_test/xpath数据解析.py
@@ -3,7 +3,7 @@ from lxml import etree
 from src.crawler_test.crawler1 import send_request
 
 if __name__ == "__main__":
-    url = "https://www.gamersky.com/news/202306/1607553.shtml"
+    url = "https://www.gamersky.com/news/202306/1608051.shtml"
     while True:
         response = send_request.send_request(method="get", url=url)
         tree = etree.HTML(response.text)
@@ -12,7 +12,7 @@ if __name__ == "__main__":
             img_data = send_request.send_request(method="get", url=img_url).content
             send_request.save(file=img_data, name=img_name)
         next_page = tree.xpath('//div[@class="page_css"]/a')[-1]
-        if next_page.xpath('./text()')[0] == "下一页":
-            url = next_page.xpath('@href')[0]
+        if next_page.xpath("./text()")[0] == "下一页":
+            url = next_page.xpath("@href")[0]
         else:
             break
